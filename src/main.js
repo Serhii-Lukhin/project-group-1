6fcbf84513c5ec8a5a2997b37918e1074eebded6
Serhii-Lukhin <124396405+Serhii-Lukhin@users.noreply.github.com>
2023-04-07 09:33:10 +0300

    js fix

--- src/main.js
+++ src/main.js
@@ -1,20 +1,2 @@
-
-import './js/our-products.js'
-=======
-/*soft scroll*/
-
-const anchors = document.querySelectorAll('a[href*="#"]');
-
-for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    const blockID = anchor.getAttribute('href').substr(1);
-
-    document.getElementById(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
-  });
-}
-
+import './js/our-products.js';
+import './js/softscroll.js';
