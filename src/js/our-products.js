4bfc8e5877769d734b9b52c35a6ea6ba175170f2
Rummut <kzantip@gmail.com>
2023-04-08 23:40:54 +0300

    edits our products section (final)

--- src/js/our-products.js
+++ src/js/our-products.js
@@ -1,19 +1,28 @@
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+    pagination: {
     el: '.swiper-pagination',
     clickable: true
   },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-});
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
+  }
+  
+})
 
