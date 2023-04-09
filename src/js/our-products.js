815e70458eac54d46f722146f65d814145baf66e
Serhii-Lukhin <124396405+Serhii-Lukhin@users.noreply.github.com>
2023-04-09 16:26:20 +0300

    our products fix

--- src/js/our-products.js
+++ src/js/our-products.js
@@ -1,28 +1,23 @@
 const swiper = new Swiper('.swiper', {
   // Default parameters
   slidesPerView: 1,
-  spaceBetween: 10,
-    pagination: {
+  spaceBetween: 18,
+  pagination: {
     el: '.swiper-pagination',
-    clickable: true
+    clickable: true,
   },
   breakpoints: {
     // when window width is >= 320px
     320: {
       slidesPerView: 1,
-      spaceBetween: 20
     },
     // when window width is >= 480px
     768: {
       slidesPerView: 2,
-      spaceBetween: 30
     },
     // when window width is >= 640px
     1200: {
       slidesPerView: 4,
-      spaceBetween: 40
-    }
-  }
-  
-})
-
+    },
+  },
+});
