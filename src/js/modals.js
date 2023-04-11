f577051be0050e672cb06194798c34bd08b86cb3
Serhii-Lukhin <124396405+Serhii-Lukhin@users.noreply.github.com>
2023-04-11 22:26:47 +0300

    fix img sellers

--- src/js/modals.js
+++ src/js/modals.js
@@ -2,14 +2,16 @@ document.addEventListener('click', function (event) {
   const widthBefor = document.documentElement.clientWidth;
   const myModal = event.target;
   const myModalId = myModal.dataset.modal;
-  const myBody = document.body;
-  myBody.classList.toggle('scroll-stop');
-  const widthAfter = document.documentElement.clientWidth;
-  const myModalForm = document.querySelector('#' + myModalId);
-  if (widthAfter > widthBefor) {
-    myBody.style.paddingRight = widthAfter - widthBefor + 'px';
-  } else {
-    myBody.removeAttribute('style');
+  if (!(myModalId == null)) {
+    const myBody = document.body;
+    myBody.classList.toggle('scroll-stop');
+    const widthAfter = document.documentElement.clientWidth;
+    const myModalForm = document.querySelector('#' + myModalId);
+    if (widthAfter > widthBefor) {
+      myBody.style.paddingRight = widthAfter - widthBefor + 'px';
+    } else {
+      myBody.removeAttribute('style');
+    }
+    myModalForm.classList.toggle('is-hidden');
   }
-  myModalForm.classList.toggle('is-hidden');
 });
